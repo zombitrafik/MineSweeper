fedeaa8a05e6a89d3bfe07f4f8522cc3408e7cbd
vladislav_mozhayskiy <vladislav.mozhayskiy@mainsoft.by>
2015-12-07 18:02:02 +0300

    add simple grunt build

--- Client/Gruntfile.js
+++ Client/Gruntfile.js
@@ -46,6 +46,131 @@ module.exports = function(grunt) {
                     '<%= mineSweeper.app %>/components/**/*.js'
                 ]
             }
+        },
+
+        copy: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'app',
+                    dest: '<%= mineSweeper.dist %>',
+                    src: [
+                        '**/*.html',
+                        'image/**/*',
+                        '!bower_components/**',
+                        '!shared/**'
+                    ]
+                }]
+            }
+        },
+
+        clean: {
+            dist: {
+                files: [{
+                    dot: true,
+                    src: [
+                        '.tmp',
+                        '<%= mineSweeper.dist %>/{,*/}*',
+                        '!<%= mineSweeper.dist %>/.git{,*/}*'
+                    ]
+                }]
+            }
+        },
+
+        useminPrepare: {
+            html: '<%= mineSweeper.app %>/index.html',
+            options: {
+                dest: '<%= mineSweeper.dist %>',
+                flow: {
+                    html: {
+                        steps: {
+                            js: ['concat', 'uglify'],
+                            css: ['cssmin']
+                        },
+                        post: {}
+                    }
+                }
+            }
+        },
+
+        usemin: {
+            html: ['<%= mineSweeper.dist %>/{,*/}*.html'],
+            css: ['<%= mineSweeper.dist %>/styles/{,*/}*.css'],
+            options: {
+                assetsDirs: [
+                    '<%= mineSweeper.dist %>',
+                    '<%= mineSweeper.dist %>/images',
+                    '<%= mineSweeper.dist %>/styles'
+                ]
+            }
+        },
+
+        concat: {
+            js: {
+                files: [
+                    {
+                        dest: '.tmp/scripts/main.js',
+                        src: [
+                            '<%= mineSweeper.app %>/components/**/*.js',
+                            '<%= mineSweeper.app %>/shared/**/*.js'
+                        ]
+                    }
+                ]
+            },
+            css :{
+                src: [
+                    'styles/*.css'
+                ],
+                dest: '.tmp/styles/main.css'
+            }
+        },
+
+        cssmin: {
+            css: {
+                src: '.tmp/styles/main.css',
+                dest: '<%= mineSweeper.dist %>/styles/main.min.css'
+            }
+        },
+
+        uglify: {
+            options: {
+                mangle: false
+            },
+
+            js: {
+                files: {
+                    '<%= mineSweeper.dist %>/scripts/main.min.js': '.tmp/scripts/main.js'
+                }
+            },
+
+            bower: {
+                options: {
+                    compress: true
+                },
+                files: {
+                    '<%= mineSweeper.dist %>/scripts/vendor.min.js': '.tmp/scripts/vendor.js'
+                }
+            }
+        },
+
+        bower_concat: {
+            options: {
+                separator: '\r\n'
+            },
+            all: {
+                dest: '.tmp/scripts/vendor.js',
+                bowerOptions: {
+                    relative: false
+                }
+            }
+        },
+
+        bower: {
+            install: {
+                options: {
+                    copy: false
+                }
+            }
         }
     });
 
@@ -53,6 +178,15 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-livereload');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-usemin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-bower-task');
+    grunt.loadNpmTasks('grunt-bower-concat');
+
 
     grunt.registerTask('serve', 'Compile then start a connect web server', function () {
         grunt.task.run([
@@ -60,4 +194,17 @@ module.exports = function(grunt) {
             'watch'
         ]);
     });
+
+    grunt.registerTask('build', [
+        'clean:dist',
+        'bower:install',
+        'useminPrepare',
+        'bower_concat',
+        'concat',
+        'uglify:js',
+        'uglify:bower',
+        'cssmin',
+        'copy',
+        'usemin'
+    ]);
 };
